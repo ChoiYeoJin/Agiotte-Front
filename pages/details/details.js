171198a10adbd3e1993f31d4042e0b3382b6e1d9
ChoiYeoJin <sonata7531@gmail.com>
2023-11-04 01:43:10 +0900

    details 기능 작업, 이미지 비율 수정

--- pages/details/details.js
+++ pages/details/details.js
@@ -3,6 +3,8 @@
 //장바구니에 저장해야하니까 전역? 상품ID, quantity
 //바로구매시
 
+import { getTagHTML } from "../utils/tags";
+
 const id = 11;
 
 const details = await fetchDetails(id);
@@ -11,7 +13,29 @@ const img = document.querySelector(".main-img");
 const subject = document.querySelector(".subject");
 const tag = document.querySelector(".tag-box");
 const price = document.querySelector(".price");
-const quantity = document.querySelector(".quantity");
+const select = document.querySelector("select");
+const total = document.querySelector(".total-price");
+const localePrice = Number(details.price).toLocaleString();
+
+img.setAttribute("src", details.img);
+
+subject.textContent = details.subject;
+
+tag.innerHTML = getTagHTML(details.tag);
+
+price.innerText = localePrice + "원";
+
+total.innerText = localePrice;
+
+if (details.quantity > 1) {
+  for (let i = 2; i < details.quantity + 1; i++) {
+    const option = document.createElement("option");
+    option.innerText = i;
+    select.appendChild(option);
+  }
+}
+
+select.addEventListener("change", changeQuantityEvent);
 
 async function fetchDetails(id) {
   //데이터를 가져온셈 치자
@@ -19,11 +43,16 @@ async function fetchDetails(id) {
   //const data = res.json();
   const details = {
     subject: "귀여운 원숭이 인형 팝니다",
-    img: "/img/test2.png",
+    img: "/imgs/test2.jpg",
     tag: "good",
     price: 40000,
     quantity: 5,
   };
 
   return details;
+}
+
+function changeQuantityEvent(e) {
+  const qnt = e.target.value;
+  total.innerText = (Number(details.price) * qnt).toLocaleString();
 }
