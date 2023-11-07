10565126166bba5c81f95ce97d713bb855348ed1
[SW7] 김정하 <huruhaa@gmail.com>
2023-11-07 11:54:11 +0900

    11.07 수정 및 js

---
+++ pages/change-pw/change-pw.js
@@ -0,0 +1,15 @@
+import * as api from "../utils/api"
+import * as storage from "../utils/storage"
+
+const loginButton = document.querySelector(".loginButton");
+  loginButton.addEventListener("click", clickLoginButton);
+  
+  function clickLoginButton(e) {
+
+    const email = document.querySelector("#password-input").value;
+    const password = document.querySelector("#passwordCheck-input").value;
+
+    storage.setItem("token", "test");
+    console.log(storage.getItem("token"));
+  }
+    
