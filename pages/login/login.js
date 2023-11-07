fa5ebbfa59076d20eacc70898485264abb69ce67
ChoiYeoJin <sonata7531@gmail.com>
2023-11-07 11:41:41 +0900

    백엔드 회의 반영사항

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,33 +1,27 @@
 //login 기능
+//1. api 임포트
 import * as api from "../utils/api";
 import * as storage from "../utils/storage";
 
+//2. 이름을 지정해둔 버튼 element를 가져온다
 const loginButtonEl = document.querySelector(".login-button");
+//3. click이벤트를 버튼에 추가한다
 loginButtonEl.addEventListener("click", clickLoginButton);
 
+//4. 추가한 이벤트를 구현한다
 async function clickLoginButton(e) {
-  e.preventDefault();
+  //5. 지정한 input element에서 .value로 입력상태의 데이터를 가져온다
   const email = document.querySelector("#emailInput").value;
   const password = document.querySelector("#passwordInput").value;
 
   storage.setItem("token", "test");
   console.log(storage.getItem("token"));
 
-  //나중에 연결되고 사용
-  /*const response = await api.sendPost("/users/login", {
+  //일단은 백엔드 보내는 부분은 제외해주세요.
+  const response = await api.sendPost("/users/login", {
     email: email,
     password: password,
   });
 
   const status = response.status;
-
-  if (status === 200) {
-    alert("login 성공!");
-    //token저장
-    const token = response.token;
-    storage.setItem("token", token);
-  } else {
-    alert(`${status} 아이디 또는 비밀번호가 틀립니다.`);
-  }
-  */
 }
