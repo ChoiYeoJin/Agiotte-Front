d842a081879585272b2e2d23fda231bfbe5bc36d
[SW7] 김정하 <huruhaa@gmail.com>
2023-11-08 13:16:14 +0900

    identity-pw js 수정

--- pages/identity-pw/identity-pw.js
+++ pages/identity-pw/identity-pw.js
@@ -6,8 +6,11 @@ const loginButtonEl = document.querySelector(".loginButton");
 loginButtonEl.addEventListener("click", clickLoginButton);
 
 function clickLoginButton(e) {
-  const id = document.querySelector("#idInput").value;
-  const email = document.querySelector("#emailInput").value;
+  const idEl = document.getElementById("idInput");
+  const emailEl = document.getElementById("emailInput");
+
+  const id = idEl.value;
+  const email = emailEl.value;
 
 if (id === '') {
   alert('아이디를 입력해주세요.');
@@ -15,19 +18,13 @@ if (id === '') {
   return false;
 }
 
-// 왜 버튼을 누르면 alert가 안뜨는지
-if (!checkEmail(email)) {
-  alert('올바른 이메일 주소를 입력해주세요.');
+if (email === '') {
+  alert('이메일을 입력해주세요.');
   emailEl.focus();
   return false;
-}
+} 
 }
 
-// 입력한 값 : idInput, emailInput
-// 가져오는 값 : 어떻게 표시하는지
-// 이 둘이 같은지 검사하기
-// api.sendPost 
-// 백엔드에 지정된 url을 통해 본인인증 확인 - 성공했는지 실패했는지 return 값이 옴
 
 
 // 제대로 입력 되었으면 비밀번호 변경페이지로 넘어가기
