206321fa18e8947116d9dd6414f7ec01209391d6
[SW7] 김정하 <huruhaa@gmail.com>
2023-11-09 19:53:40 +0900

    pw 변경 및 인증, profile 인증, id 찾기 JS 수정

--- pages/find-id/find-id.js
+++ pages/find-id/find-id.js
@@ -1,17 +1,17 @@
 import * as api from "../utils/api"
 import * as storage from "../utils/storage"
 
-const API_URL = 'http://kdt-sw-7-team02.elicecoding.com:3000/images/bmo1.gif';
+const API_URL = 'http://kdt-sw-7-team02.elicecoding.com/api';
 
 const findButtonEl = document.querySelector(".findButton");
-findButtonEl.addEventListener("click", clickfindButton);
+findButtonEl.addEventListener("click", clickFindButton);
 
 const emailRegexp = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 const checkEmailValid = (targetEmail) => {
     return emailRegexp.test(targetEmail);
 }
 
-function clickfindButton(e) {
+function clickFindButton(e) {
 
   const nameEl = document.querySelector("#nameInput");
   const name = nameEl.value;
@@ -42,23 +42,27 @@ const params = {
   "Email": "String"
 };
 
-fetch({
-  url: `${API_URL}/users/id`,
+fetch(`${API_URL}/users/id`, {
   method: 'POST',
   headers: {
       "Content-Type": "application/json",
   },
   body: JSON.stringify(params),
 })
+
 .then(res => {
-  console.log(res, res.ok, res.status);
+  console.log(res, res.ok, res.text, res.status);
   
   if(res.ok) {
-    alert('회원님의 아이디는'+ String +'입니다.');
+    alert(response.text());
+    location.href = `/login/`
   } else {
     alert('이름이나 이메일이 일치하지 않습니다.');
+  // } else {
+  //   alert('회원정보가 존재하지 않습니다.')
   }
 })
 .catch(err => console.error(err));
-// alert('등록된 정보가 없습니다')
 }
+
+
