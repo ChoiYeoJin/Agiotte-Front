1ee4e07624a002c08cb69cd52c5be27a0a335a18
zero86 <katanazero86@gmail.com>
2023-11-09 20:38:07 +0900

    fix
    인증 부분 코드 수정

--- pages/profile-verification/profile-verification.js
+++ pages/profile-verification/profile-verification.js
@@ -8,43 +8,52 @@ verificationButtonEl.addEventListener("click", clickVerificationButton);
 
 function clickVerificationButton(e) {
 
-  const idEl = document.querySelector("#idInput");
-  const id = idEl.value;
-  const passwordEl = document.querySelector("#passwordInput");
-  const password = passwordEl.value;
+    const idEl = document.querySelector("#idInput");
+    const id = idEl.value;
+    const passwordEl = document.querySelector("#passwordInput");
+    const password = passwordEl.value;
 
-  if (id === '') {
-    alert('아이디를 입력해주세요.');
-    idEl.focus();
-    return false;
-  }
-  if (password === '') {
-    alert('비밀번호를 입력해주세요.');
-    passwordEl.focus();
-    return false;
-  }
+    if (id === '') {
+        alert('아이디를 입력해주세요.');
+        idEl.focus();
+        return false;
+    }
+    if (password === '') {
+        alert('비밀번호를 입력해주세요.');
+        passwordEl.focus();
+        return false;
+    }
 
-  // API Request
-  const params = {
-    UserId: String,
-    HashPwd: String,
-  };
+    // API Request
+    const params = {
+        UserId: id,
+        HashPwd: password,
+    };
 
-  fetch(`${API_URL}/users`, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
-  })
-    .then(res => {
-      console.log(res, res.ok, res.status);
+    const token = storage.getItem('token');
 
-      if (res.ok) {
-        location.href = '/profile/';
-      } else {
-        alert('아이디나 비밀번호가 일치하지 않습니다.');
-      }
-    })
-    .catch(err => console.error(err));
+    if (token) {
+        fetch(`${API_URL}/users`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `${token}`
+            },
+            body: JSON.stringify(params),
+        })
+            .then(res => {
+                console.log(res, res.ok, res.status);
+
+                if (res.ok) {
+                    location.href = '/profile/';
+                } else {
+                    alert('아이디나 비밀번호가 일치하지 않습니다.');
+                }
+            })
+            .catch(err => console.error(err));
+    } else {
+        alert('유효하지 않은 사용자 입니다.');
+        location.href = '/login/';
+    }
+
 }
