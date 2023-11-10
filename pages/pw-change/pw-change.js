2cd3b71a0386fd698f3e62d47f6dae6e6e35b61f
zero86 <katanazero86@gmail.com>
2023-11-10 21:13:56 +0900

    fix 패스워드 변경 부분 코드 수정

--- pages/pw-change/pw-change.js
+++ pages/pw-change/pw-change.js
@@ -18,61 +18,54 @@ changeButtonEl.addEventListener("click", clickChangeButton);
 
 const passwordRegexp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 const checkPwValid = (targetPassword) => {
-  return passwordRegexp.test(targetPassword);
+    return passwordRegexp.test(targetPassword);
 }
 
 function clickChangeButton(e) {
 
-  const passwordEl = document.querySelector("#password-input");
-  const password = passwordEl.value;
-  const passwordCheckEl = document.querySelector("#passwordCheck-input");
-  const passwordCheck = passwordCheckEl.value;
+    const passwordEl = document.querySelector("#password-input");
+    const password = passwordEl.value;
+    const passwordCheckEl = document.querySelector("#passwordCheck-input");
+    const passwordCheck = passwordCheckEl.value;
 
-  if (password === '') {
-    alert('비밀번호를 입력해주세요.');
-    passwordEl.focus();
-    return false;
-  }
-  if (passwordCheck === '') {
-    alert('비밀번호 확인을 입력해주세요.');
-    passwordCheckEl.focus();
-    return false;
-  }
-  if (!checkPwValid(password)) {
-    alert('올바른 비밀번호를 입력해주세요!\n비밀번호는 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자를 모두 포함해야 합니다.');
-    password.focus();
-    return false;
-  }
-  if(password !== passwordCheck) {
-    alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
-    return false;
-  }
-
-
-  // API Request
-  const params = {
-    HashPwd: password,
-    UserId,
-  };
-
-  fetch(`${API_URL}/users/password/:id`, {
-    method: 'PUT',
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(params),
-  })
-    .then(res => {
-      console.log(res, res.ok, res.status);
-
-      if (res.ok) {
-        alert('비밀번호가 변경되었습니다.');
-        location.href = `/login/`;
-
-      } else {
-        alert('비밀번호가 일치하지 않습니다.');
+    if (password === '') {
+        alert('비밀번호를 입력해주세요.');
+        passwordEl.focus();
+        return false;
+    }
+    if (passwordCheck === '') {
+        alert('비밀번호 확인을 입력해주세요.');
+        passwordCheckEl.focus();
+        return false;
+    }
+    if (!checkPwValid(password)) {
+        alert('올바른 비밀번호를 입력해주세요!\n비밀번호는 8자 이상이어야 하며, 대문자, 소문자, 숫자, 특수문자를 모두 포함해야 합니다.');
         password.focus();
-      }
+        return false;
+    }
+    if (password !== passwordCheck) {
+        alert('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+        return false;
+    }
+
+    fetch(`${API_URL}/users/password/${UserId}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            HashPwd: password
+        }),
     })
-    .catch(err => console.error(err));
+        .then(res => {
+            console.log(res, res.ok, res.status);
+            if (res.ok) {
+                alert('비밀번호가 변경되었습니다.');
+                location.href = `/login/`;
+            } else {
+                alert('비밀번호가 일치하지 않습니다.');
+                password.focus();
+            }
+        }).then(data => console.log(data))
+        .catch(err => console.error(err));
 }
